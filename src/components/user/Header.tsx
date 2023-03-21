7f25de3c4973e3761551f90f89a01bbf18f07bfb
aboveStars <93012417+aboveStars@users.noreply.github.com>
2023-03-22 00:09:59 +0300

    More portion of 'Profile Page' functionality has been written.

--- src/components/user/Header.tsx
+++ src/components/user/Header.tsx
@@ -1,23 +1,23 @@
 import {
   Button,
-  ButtonGroup,
   Divider,
   Flex,
   Icon,
   Image,
   Input,
+  Stack,
   Text,
 } from "@chakra-ui/react";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   currentUserStateAtom,
   UserDataAtUserpage,
 } from "../atoms/currentUserAtom";
 
+import useImageUpload from "@/hooks/useImageUpload";
+import { AiFillEdit, AiOutlineUpload } from "react-icons/ai";
 import { CgProfile } from "react-icons/cg";
 import { useRecoilValue } from "recoil";
-import { AiFillEdit, AiOutlineUpload } from "react-icons/ai";
-import useImageUpload from "@/hooks/useImageUpload";
 
 type Props = {
   userData: UserDataAtUserpage;
@@ -27,10 +27,21 @@ export default function Header({ userData }: Props) {
   const currentUserUid = useRecoilValue(currentUserStateAtom).uid;
 
   const [canModify, setCanModify] = useState<boolean>(false);
+  const [modifying, setModifying] = useState<boolean>(false);
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const { selectedFile, onSelectFile } = useImageUpload();
+  /**
+   * setSelectedFile for cancelling pp changing (clearing state)
+   */
+  const {
+    selectedFile,
+    onSelectFile,
+    setSelectedFile,
+    profilePhotoUpload,
+    profilePhotoUploadError,
+    profilePhotoUploadLoading,
+  } = useImageUpload();
 
   /**
    * userData is already being controlled then, comes here
@@ -50,27 +61,72 @@ export default function Header({ userData }: Props) {
 
   return (
     <Flex direction="column" justify="center" align="center">
-      {userData.profilePhoto || selectedFile ? (
-        <Image src={selectedFile} height="200px" rounded="full" />
-      ) : (
-        <>
-          <Icon as={CgProfile} color="white" fontSize="8xl" />
-        </>
-      )}
-      {canModify && (
-        <>
-          <Button
-            width="10px"
-            height="15px"
-            onClick={() => {
-              inputRef.current?.click();
-            }}
-          >
-            <Icon as={AiOutlineUpload} width="15px" height="15px" />
-          </Button>
-          <Input ref={inputRef} type="file" hidden onChange={onSelectFile} />
-        </>
-      )}
+      <Flex justify="center" mt={2}>
+        {selectedFile ? (
+          <>
+            <Image src={selectedFile} height="200px" rounded="full" />
+          </>
+        ) : userData.profilePhoto ? (
+          <>
+            <Image src={userData.profilePhoto} height="200px" rounded="full" />
+          </>
+        ) : (
+          <Icon as={CgProfile} color="white" height="200px" width="200px" />
+        )}
+      </Flex>
+
+      <Flex mt={5} mb={5}>
+        {canModify && (
+          <>
+            {!modifying && (
+              <Icon
+                as={AiOutlineUpload}
+                cursor="pointer"
+                onClick={() => {
+                  inputRef.current?.click();
+                }}
+                color="white"
+                fontSize="2xl"
+              />
+            )}
+
+            <Input
+              ref={inputRef}
+              type="file"
+              hidden
+              onChange={(event) => {
+                setModifying(true);
+                onSelectFile(event);
+              }}
+            />
+
+            {modifying && (
+              <Stack direction="row" gap={1}>
+                <Button
+                  size="sm"
+                  onClick={async () => {
+                    await profilePhotoUpload();
+                    setModifying(false);
+                  }}
+                  isLoading={profilePhotoUploadLoading}
+                >
+                  Save Profile Photo
+                </Button>
+                <Button
+                  onClick={() => {
+                    setSelectedFile("");
+                    setModifying(false);
+                  }}
+                  size="sm"
+                  isDisabled={profilePhotoUploadLoading}
+                >
+                  Cancel
+                </Button>
+              </Stack>
+            )}
+          </>
+        )}
+      </Flex>
 
       <Text textColor="white">{userData.username}</Text>
       <Divider />
--- src/hooks/useImageUpload.tsx
+++ src/hooks/useImageUpload.tsx
@@ -1,8 +1,29 @@
+import { currentUserStateAtom } from "@/components/atoms/currentUserAtom";
+import { firestore, storage } from "@/firebase/clientApp";
+import { doc, serverTimestamp, updateDoc } from "firebase/firestore";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadString,
+} from "firebase/storage";
 import { useState } from "react";
+import { useRecoilState } from "recoil";
 
 export default function useImageUpload() {
   const [selectedFile, setSelectedFile] = useState<string>("");
 
+  const [currentUserState, setCurrentUserState] =
+    useRecoilState(currentUserStateAtom);
+
+  const [profilePhotoUploadLoading, setProfilePhotoUploadLoading] =
+    useState(false);
+  const [profilePhotoUploadError, setProfilePhotoUploadError] = useState(false);
+  /**
+   * Selecting image efficiently, produce ready data to write storage via selectedFile
+   * @param event
+   * @returns
+   */
   const onSelectFile = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!event.target.files) {
       console.log("No Files Provided to onSelectFile \n aborting.....");
@@ -17,11 +38,111 @@ export default function useImageUpload() {
     reader.onload = (readerEvent) => {
       setSelectedFile(readerEvent.target?.result as string);
     };
-    
+  };
+
+  /**
+   * This is the main image upload function.
+   *
+   */
+  const profilePhotoUpload = async () => {
+    let errorHappened: boolean = false;
+    setProfilePhotoUploadError(false);
+
+    setProfilePhotoUploadLoading((prev) => true);
+
+    const username = currentUserState.username;
+    const fileName = Date.now();
+    // client => storage
+    const imageRef = ref(
+      storage,
+      `users/${username}/profilePhotos/${fileName}`
+    );
+
+    try {
+      await uploadString(imageRef, selectedFile, "data_url");
+    } catch (error) {
+      console.log("Error while uploading pp to storage: ", error);
+      errorHappened = true;
+    }
+
+    if (errorHappened) {
+      setProfilePhotoUploadLoading(false);
+      setProfilePhotoUploadError(true);
+      return;
+    }
+
+    // Check Upload result
+
+    // AFter here, now we adding image soruce to firestore (storage => firestore)
+    let downloadableImageURL: string = "";
+
+    try {
+      downloadableImageURL = await getDownloadURL(imageRef);
+    } catch (error) {
+      console.log("Error while creating 'downloadURL' : ", error);
+      errorHappened = true;
+    }
+
+    if (errorHappened) {
+      console.log(
+        "Due to an error at 'creation of downloadURL' we are taking back all stuff we did"
+      );
+
+      console.log("Deleting image from 'firebase/storage'");
+      await deleteObject(imageRef);
+      console.log("Deletaion successfull");
+
+      setProfilePhotoUploadLoading(false);
+      setProfilePhotoUploadError(true);
+      return;
+    }
+
+    const firestoreDocRef = doc(firestore, `users/${username}`);
+
+    try {
+      await updateDoc(firestoreDocRef, {
+        profilePhoto: downloadableImageURL,
+      });
+    } catch (error) {
+      console.log(
+        "Error while putting downloadable Image URL to Firestore:  ",
+        error
+      );
+      errorHappened = true;
+    }
+
+    if (errorHappened) {
+      console.log(
+        "Due to error at putting URL to firestore, we are taking back all stuff we did"
+      );
+
+      console.log("Deleting image from 'firebase/storage'");
+      await deleteObject(imageRef);
+      console.log("Deletaion successfull");
+
+      setProfilePhotoUploadLoading(false);
+      setProfilePhotoUploadError(true);
+      return;
+    }
+
+    console.log("Profile Photo Uploading Successfull");
+
+    // State Updates
+    // (until refresh, user normally will not be able to see its new image so we change it manually)
+    setCurrentUserState((prev) => ({
+      ...prev,
+      profilePhoto: downloadableImageURL,
+    }));
+
+    setProfilePhotoUploadLoading(false);
   };
 
   return {
     selectedFile,
     onSelectFile,
+    setSelectedFile,
+    profilePhotoUpload,
+    profilePhotoUploadLoading,
+    profilePhotoUploadError,
   };
 }
