2393928ae968a15ab55d7140a75e340ae16f3631
aboveStars <93012417+aboveStars@users.noreply.github.com>
2023-04-02 18:39:41 +0300

    Error handling while deploying Vercel

--- src/components/system-status/SystemStatus.tsx
+++ src/components/system-status/SystemStatus.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-type Props = {};
-
-export default function SystemStatus({}: Props) {
+export default function SystemStatus() {
   return (
     <div>
-      <script src="https://blocksocial.statuspage.io/embed/script.js"></script>
+      <script
+        async
+        src="https://blocksocial.statuspage.io/embed/script.js"
+      ></script>
     </div>
   );
 }
