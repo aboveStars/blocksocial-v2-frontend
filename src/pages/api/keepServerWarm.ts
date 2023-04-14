080e6678554344d19711f902cc4b249a8c94f63f
aboveStars <93012417+aboveStars@users.noreply.github.com>
2023-04-14 16:00:01 +0300

    Modified 'keepServerWarm' API

--- src/pages/api/keepServerWarm.ts
+++ src/pages/api/keepServerWarm.ts
@@ -4,17 +4,16 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("Keep Warm Fired!");
 
   console.log(req);
-  // fetch("https:/blocksocial.vercel.app/api/follow");
-  // fetch("https:/blocksocial.vercel.app/api/postComments");
-  // fetch("https:/blocksocial.vercel.app/api/postCommentsDelete");
-  // fetch("https:/blocksocial.vercel.app/api/postDelete");
-  // fetch("https:/blocksocial.vercel.app/api/postLike");
-  // fetch("https:/blocksocial.vercel.app/api/postUplaod");
-  // fetch("https:/blocksocial.vercel.app/api/profilePhotoChange");
-  // fetch("https:/blocksocial.vercel.app/api/refreshNFT");
-  // fetch("https:/blocksocial.vercel.app/api/signup");
-  // fetch("https:/blocksocial.vercel.app/api/update");
-  fetch("/api/follow");
+  fetch("https:/blocksocial.vercel.app/api/follow");
+  fetch("https:/blocksocial.vercel.app/api/postComments");
+  fetch("https:/blocksocial.vercel.app/api/postCommentsDelete");
+  fetch("https:/blocksocial.vercel.app/api/postDelete");
+  fetch("https:/blocksocial.vercel.app/api/postLike");
+  fetch("https:/blocksocial.vercel.app/api/postUplaod");
+  fetch("https:/blocksocial.vercel.app/api/profilePhotoChange");
+  fetch("https:/blocksocial.vercel.app/api/refreshNFT");
+  fetch("https:/blocksocial.vercel.app/api/signup");
+  fetch("https:/blocksocial.vercel.app/api/update");
 
   return res.status(200).json({
     status: "Server Warmed",
