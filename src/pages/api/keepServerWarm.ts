20e76ad7b616787dda7f8e3ff8bb75a38678584f
aboveStars <93012417+aboveStars@users.noreply.github.com>
2023-04-14 16:17:00 +0300

    Modified 'keepServerWarm' API

--- src/pages/api/keepServerWarm.ts
+++ src/pages/api/keepServerWarm.ts
@@ -1,16 +1,24 @@
 import { NextApiResponse } from "next";
 
 export default async function handler(res: NextApiResponse) {
-  await fetch("https:/blocksocial.vercel.app/api/follow");
-  await fetch("https:/blocksocial.vercel.app/api/postComments");
-  await fetch("https:/blocksocial.vercel.app/api/postCommentsDelete");
-  await fetch("https:/blocksocial.vercel.app/api/postDelete");
-  await fetch("https:/blocksocial.vercel.app/api/postLike");
-  await fetch("https:/blocksocial.vercel.app/api/postUplaod");
-  await fetch("https:/blocksocial.vercel.app/api/profilePhotoChange");
-  await fetch("https:/blocksocial.vercel.app/api/refreshNFT");
-  await fetch("https:/blocksocial.vercel.app/api/signup");
-  await fetch("https:/blocksocial.vercel.app/api/update");
+  const warmUps = [
+    fetch("https:/blocksocial.vercel.app/api/follow"),
+    fetch("https:/blocksocial.vercel.app/api/postComments"),
+    fetch("https:/blocksocial.vercel.app/api/postCommentsDelete"),
+    fetch("https:/blocksocial.vercel.app/api/postDelete"),
+    fetch("https:/blocksocial.vercel.app/api/postLike"),
+    fetch("https:/blocksocial.vercel.app/api/postUplaod"),
+    fetch("https:/blocksocial.vercel.app/api/profilePhotoChange"),
+    fetch("https:/blocksocial.vercel.app/api/refreshNFT"),
+    fetch("https:/blocksocial.vercel.app/api/signup"),
+    fetch("https:/blocksocial.vercel.app/api/update"),
+  ];
+
+  try {
+    await Promise.all(warmUps);
+  } catch (error) {
+    console.error("Warm-Up is done. We got error as normal.");
+  }
 
   return res.status(200).json({
     status: "Server Warmed",
