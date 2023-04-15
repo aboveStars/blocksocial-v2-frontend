8004942f047ff98e85bfa00749eccbef1874700c
aboveStars <93012417+aboveStars@users.noreply.github.com>
2023-04-15 17:25:59 +0300

    'postCommentDelete' API is refactored.

--- src/hooks/useCommentDelete.tsx
+++ src/hooks/useCommentDelete.tsx
@@ -1,27 +1,33 @@
 import { auth } from "@/firebase/clientApp";
 import { useState } from "react";
-import safeJsonStringify from "safe-json-stringify";
 
 export default function useCommentDelete() {
   const [commentDeletionLoading, setCommentDeletionLoading] = useState(false);
 
   const commentDelete = async (userCommentDocPath: string) => {
-    if (!!!userCommentDocPath) {
+    if (!userCommentDocPath) {
       return;
     }
-
     setCommentDeletionLoading(true);
 
     const fullPath = userCommentDocPath;
     const subStringtoDeleteIndex = fullPath.indexOf("comments");
     const postDocPath = fullPath.substring(0, subStringtoDeleteIndex);
 
-    console.log("Comment Deletion is started");
-
-    const idToken = await auth.currentUser?.getIdToken();
-
+    let idToken = "";
     try {
-      const response = await fetch("/api/postCommentDelete", {
+      idToken = (await auth.currentUser?.getIdToken()) as string;
+    } catch (error) {
+      console.error(
+        "Error while comment deleting. Couln't be got idToken",
+        error
+      );
+      return;
+    }
+
+    let response: Response;
+    try {
+      response = await fetch("/api/postCommentDelete", {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
@@ -32,17 +38,19 @@ export default function useCommentDelete() {
           postDocPath: postDocPath,
         }),
       });
-
-      if (!response.ok) {
-        const { error } = await response.json();
-        throw new Error(safeJsonStringify(error));
-      } else {
-        console.log("Comment Deletion is succesfull");
-        setCommentDeletionLoading(false);
-      }
     } catch (error) {
-      console.error("Error while deleting comment", error);
+      console.error("Error while fetching 'postCommentDelete' API", error);
+      return;
     }
+
+    if (!response.ok) {
+      console.error(
+        "Error while deleting comment from postCommentDelete API",
+        await response.json()
+      );
+      return;
+    }
+    setCommentDeletionLoading(false);
   };
   return {
     commentDelete,
--- src/pages/api/postCommentDelete.ts
+++ src/pages/api/postCommentDelete.ts
@@ -1,3 +1,4 @@
+import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { auth, firestore, fieldValue } from "../../firebase/adminApp";
@@ -14,43 +15,102 @@ export default async function handler(
     return res.status(200).json({ status: "Follow fired by Cron" });
   }
 
-  if (!authorization || !authorization.startsWith("Bearer ")) {
-    console.error("Non-User Request");
+  let decodedToken: DecodedIdToken;
+  try {
+    decodedToken = await verifyToken(authorization as string);
+  } catch (error) {
+    console.error("Error while verifying token", error);
     return res.status(401).json({ error: "Unauthorized" });
   }
+
+  let operationFromUsername = "";
   try {
-    const idToken = authorization.split("Bearer ")[1];
-    const decodedToken = await auth.verifyIdToken(idToken);
-    const uid = decodedToken.uid;
-    const displayName = (await auth.getUser(uid)).displayName;
-
-    let deleteRequestSender = displayName;
-
-    if (req.method === "DELETE") {
-      if (!commentDocPath || !postDocPath) {
-        throw new Error("Missing Prop");
-      }
-
-      const ss = await firestore.doc(commentDocPath).get();
-      if (!ss.exists) {
-        throw new Error("Could't be accessed the comment, comment not exists");
-      }
-
-      if (ss.data()?.commentSenderUsername !== deleteRequestSender) {
-        throw new Error("Not-Owner of the comment");
-      }
-
-      await firestore.doc(commentDocPath).delete();
-      await firestore.doc(postDocPath).update({
-        commentCount: fieldValue.increment(-1),
-      });
-
-      res.status(200).json({});
-    } else {
-      res.status(405).json({ error: "Method not allowed" });
-    }
+    operationFromUsername = await getDisplayName(decodedToken);
   } catch (error) {
-    console.error("Error while deleting comment", error);
-    res.status(401).json({ error: error });
+    console.error("Error while getting display name", error);
+    return res.status(401).json({ error: "Unautorized" });
+  }
+
+  if (req.method !== "DELETE")
+    return res.status(405).json("Method not allowed");
+
+  if (!commentDocPath || !postDocPath) {
+    return res.status(422).json({ error: "Invalid prop or props" });
+  }
+
+  let isOwner = false;
+  try {
+    isOwner = await isOwnerOfComment(commentDocPath, operationFromUsername);
+  } catch (error) {
+    console.error(
+      "Error while deleting comment from 'isOwner' function",
+      error
+    );
+    return res.status(503).json({ error: "Firebase error" });
+  }
+
+  if (!isOwner) {
+    console.error("Not owner of the comment");
+    return res.status(522).json({ error: "Not-Owner" });
+  }
+
+  try {
+    await Promise.all([
+      commentDelete(commentDocPath),
+      commentCountUpdate(postDocPath),
+    ]);
+  } catch (error) {
+    console.error(error);
+    return res.status(503).json({ error: "Firebase error" });
+  }
+
+  return res.status(200).json({});
+}
+
+/**
+ * @param authorization
+ * @returns
+ */
+async function verifyToken(authorization: string) {
+  const idToken = authorization.split("Bearer ")[1];
+  const decodedToken = await auth.verifyIdToken(idToken);
+  return decodedToken;
+}
+
+/**
+ * @param decodedToken
+ */
+async function getDisplayName(decodedToken: DecodedIdToken) {
+  const uid = decodedToken.uid;
+  const displayName = (await auth.getUser(uid)).displayName;
+  return displayName as string;
+}
+
+async function isOwnerOfComment(
+  commentDocPath: string,
+  operationFromUsername: string
+) {
+  const ss = await firestore.doc(commentDocPath).get();
+  return ss.data()?.commentSenderUsername === operationFromUsername;
+}
+
+async function commentDelete(commentDocPath: string) {
+  try {
+    await firestore.doc(commentDocPath).delete();
+  } catch (error) {
+    throw new Error(
+      `Error while deleting comment from 'commentDelete' function: ${error}`
+    );
+  }
+}
+async function commentCountUpdate(postDocPath: string) {
+  try {
+    await firestore.doc(postDocPath).update({
+      commentCount: fieldValue.increment(-1),
+    });
+  } catch (error) {
+    throw new Error(
+      `Error while deleting comment from 'commentCountUpdate' function: ${error}`
+    );
   }
 }
