6d200bd136b11da25b99570c51cad23e0c528878
aboveStars <93012417+aboveStars@users.noreply.github.com>
2023-05-02 01:39:23 +0300

    Minor alterations have been executed.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,26 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import Head from "next/head";
-import Layout from "@/components/Layout/Layout";
 import { theme } from "@/components/chakra/theme";
-import { RecoilRoot } from "recoil";
-import NextNProgress from "nextjs-progressbar";
+import Layout from "@/components/Layout/Layout";
+import { ChakraProvider } from "@chakra-ui/react";
 import { Analytics } from "@vercel/analytics/react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import NextNProgress from "nextjs-progressbar";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <>
       <Analytics />
       <RecoilRoot>
         <ChakraProvider theme={theme}>
           <Head>
-            <title>BlockSocial</title>
+            <title>
+              {router.asPath === "/"
+                ? "BlockSocial"
+                : `${router.asPath.split("/")[1]} - BlockSocial`}
+            </title>
             <link rel="icon" href="/bsicon.ico" />
           </Head>
           <Layout>
